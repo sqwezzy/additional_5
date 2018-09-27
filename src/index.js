296f2481abc9563f28848829f46ad493849af96d
sqwezzy <maks.subbotko46@gmail.com>
2018-09-27 22:33:58 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check (str, bracketsConfig) {
+  var arr = []
+  var open = []
+  var close = []
+  bracketsConfig.forEach(x => {
+    open.push(x[0])
+    close.push(x[1])
+  })
+  for (let i = 0; i < str.length; i++) {
+    if (open.includes(str[i])) {
+      if (close.includes(str[i]) && arr.includes(str[i]))
+        arr.pop()
+      else
+        arr.push(str[i])
+    }
+    else if (close.includes(str[i]) && arr[arr.length - 1] == open[close.indexOf(str[i])])
+      arr.pop()
+  }
+  return arr.length == 0
 }
